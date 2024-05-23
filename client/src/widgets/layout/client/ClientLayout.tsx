625faeb4e2d36e88c7e999aae92845f624a66048
S1GHE <golikovefim65@gmail.com>
2024-05-23 14:18:01 +0300

    FIX: base styles

--- client/src/widgets/layout/client/ClientLayout.tsx
+++ client/src/widgets/layout/client/ClientLayout.tsx
@@ -1,13 +1,16 @@
 import {Outlet} from "react-router";
 import {Header, Footer} from "@src/widgets/layout/client/section";
+import {ContainerModule} from "@src/shared/scss";
 
 export const ClientLayout = () => {
   return (
     <>
       <Header/>
 
-      <main>
-        <Outlet/>
+      <main className={ContainerModule.container}>
+        <div className={ContainerModule.wrapper}>
+          <Outlet/>
+        </div>
       </main>
 
       <Footer/>
