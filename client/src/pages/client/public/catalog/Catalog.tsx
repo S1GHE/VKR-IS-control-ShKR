e60061be9dd85eae4f4dd429622d3ec649a09894
s1ghe <golikovefim65@gmail.com>
2024-06-03 10:18:01 +0300

    add: courses desc

--- client/src/pages/client/public/catalog/Catalog.tsx
+++ client/src/pages/client/public/catalog/Catalog.tsx
@@ -70,12 +70,27 @@ export const Catalog = () => {
           </div> :
           <>
             <div className={cls.cat_container}>
-              <MainBtn state={"black"} onClick={() => setBottomOpen(!bottomOpen)}>Категории</MainBtn>
+              <MainBtn state={"black"} onClick={() => setBottomOpen(!bottomOpen)}>
+                <p className={TextModule.p_14}>
+                  Направления
+                </p>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="bi bi-list-task" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd"
+                        d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5zM3 3H2v1h1z"/>
+                  <path
+                    d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5M5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1z"/>
+                  <path fill-rule="evenodd"
+                        d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5zM2 7h1v1H2zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm1 .5H2v1h1z"/>
+                </svg>
+              </MainBtn>
             </div>
             <BottomModal isOpen={bottomOpen} setIsOpen={setBottomOpen}>
               <div className={cls.categories_modal}>
                 {categories.map((el) => (
-                  <MainBtn state={"gray"} to={`/catalog/${el.ID}`} onClick={() => setBottomOpen(!bottomOpen)} key={el.ID}>
+                  <MainBtn state={"gray"} to={`/catalog/${el.ID}`} onClick={() => setBottomOpen(!bottomOpen)}
+                           key={el.ID}>
                     <p className={TextModule.p_16}>{el.Name}</p>
                   </MainBtn>
                 ))}
